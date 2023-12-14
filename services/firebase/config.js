a9022493188fe98f1b489b4e9838bb48eed823b7
Toon Robeyns <toon.robeyns@routty.eu>
2023-12-14 11:46:00 +0100

    Update cache (2)

--- services/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyBCSCearwVJ31XgtB0Md-AMugy_wlf6wYY",
-  authDomain: "coin-tracker-802c7.firebaseapp.com",
-  databaseURL: "https://coin-tracker-802c7.firebaseio.com",
-  projectId: "coin-tracker-802c7",
-  storageBucket: "coin-tracker-802c7.appspot.com",
-  messagingSenderId: "680288320163",
-  appId: "1:680288320163:web:292b2e0449dfcf3eff1d16"
-}
