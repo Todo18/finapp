82b6c5dc33a7e42d160d2abe6c6321fce344f54f
Toon Robeyns <toon.robeyns@routty.eu>
2023-12-31 13:17:11 +0100

    Added search support for rules

--- components/rules/page/List.vue
+++ components/rules/page/List.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import type { ComputedRef } from 'vue'
+import type { RuleID, RuleItem } from '~/components/rules/types'
+
 const { $store, nuxt2Context: { i18n } } = useNuxtApp()
-const rulesRootIds = computed(() => $store.getters['rules/rulesRootIds'])
+const rules = $store.state.rules.items
+const rulesRootIds: ComputedRef<RuleID[]> = computed(() => $store.getters['rules/rulesRootIds'])
 
 // Clear any leftovers
 $store.commit('rules/setNewRuleFromTrn')
+
+const searchInput = ref('')
+const list = computed(() => {
+  if (searchInput.value) {
+    const needle = searchInput.value.toLowerCase()
+    return rulesRootIds.value.filter((ruleId: RuleID) => {
+        const rule: RuleItem = rules[ruleId]
+        return rule.name.toLowerCase().includes(needle) || rule.condition.toLowerCase().includes(needle)
+    })
+  }
+
+  return rulesRootIds.value
+})
 
 useHead({
   title: i18n.t('rules.title'),
@@ -18,10 +35,17 @@ UiPage
       UiHeaderLink(@click="$router.push('/rules/new')")
         UiIconAdd.w-5.h-5.group-hover_text-white
 
+  .px-2
+    input.w-full.m-0.py-2.px-3.rounded-lg.text-base.font-normal.text-item-base.bg-item-main-bg.border.border-solid.border-item-main-hover.placeholder_text-item-base-down.transition.ease-in-out.focus_text-item-base-up.focus_bg-item-main-hover.focus_border-blue3.focus_outline-none(
+      placeholder="Search..."
+      v-model="searchInput"
+      type="text"
+    )
+
   //- List
   .pt-1.pb-12.px-2
     RulesList(
-      :ids="rulesRootIds"
+      :ids="list"
       @onClick="ruleId => $router.push(`/rules/${ruleId}`)"
     )
 
