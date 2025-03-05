92e743ea706e85659202c65b68a46618d2a57c11
Toon Robeyns <porfavor19@hotmail.com>
2025-03-05 21:07:43 +0100

    Preliminary work in Coda API

--- api/coda/index.js
+++ api/coda/index.js
@@ -1,11 +1,19 @@
-// import { processFile } from 'file:///C:/Users/robeyto/Projecten/CoinTracker/coda/daemon.mjs'
+import { processFile, loadSettings, stop } from '../../../finapp-coda/worker.mjs'
+
+async function process(filename, options) {
+  // FIXME: Call only once during startup of server
+  await loadSettings();
+  await processFile(filename, options);
+  // FIXME: Don't call this in server mode
+  await stop();
+}
 
 export default function (req, res, next) {
-  // req is the Node.js http request object
-  console.log(req.url)
+  const contentDisposition = req.headers['content-disposition']
+  const fileNameMatch = contentDisposition.match(/filename="(.+)"/)
+  const fileName = fileNameMatch ? fileNameMatch[1] : 'unknown'
 
   let fileContents = ''
-  const fileName = /* new URL(req.url, 'http://localhost/').searchParams.get('fileName') ?? */req.headers.contentName
 
   // res is the Node.js http response object
   // Get the data as utf8 strings.
@@ -16,15 +24,16 @@ export default function (req, res, next) {
   req.on('data', chunk => fileContents += chunk)
 
   // The 'end' event indicates that the entire body has been received.
-  req.on('end', () => {
+  req.on('end', async () => {
     try {
-      // const result = processFile(fileName, { contents: fileContents })
+      const result = await process(fileName, { contents: fileContents })
       res.writeHead(200, { 'Content-Type': 'application/json' })
       res.end(JSON.stringify({
         description: `Hello, ${fileName}!`,
       }))
     }
     catch (er) {
+      console.error(er)
       // uh oh! bad json!
       res.writeHead(500, { 'Content-Type': 'application/json' })
       res.end(JSON.stringify({
